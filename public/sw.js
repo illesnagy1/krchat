aff3de7a698dbc0f6739d11cf86a88baef3c40b9
Illés <illesnagy@edu.bme.hu>
2025-11-10 14:54:33 +0100

    push

--- public/sw.js
+++ public/sw.js
@@ -8,6 +8,16 @@ async function impl(e) {
         let networkResponse = await fetch(e.request); // Ha nincs meg, akkor elindítjuk a tényleges hálózati lekérdezést 
         cache.put(e.request, networkResponse.clone()) // Eltároljuk 
         return networkResponse; // Visszadjuk 
-    } 
-} 
+    }
+}
+async function pushHandler(e) { 
+    let data = e.data?.text() ?? "No payload"; 
+    e.waitUntil( 
+        self.registration.showNotification("Chat Notification", { 
+            body: data, 
+            icon: "icon.png" 
+        }) 
+    ); 
+}
 self.addEventListener("fetch", e => e.respondWith(impl(e))); // Eseményre feliratkozás
+self.addEventListener("push", pushHandler);
